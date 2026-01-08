a3755e53b9d95caa486e3f82bfbe539c700a0798
Krish Singh <154358180+krishsingh120@users.noreply.github.com>
2026-01-08 21:01:05 +0530

    Added HashSets in redis

---
+++ redisHash.js
@@ -0,0 +1,35 @@
+const redisClient = require("./redisClient");
+
+// on "connect" -> event
+redisClient.on("connect", () => {
+  console.log("Redis is connected");
+});
+
+// on "disconnect" -> event
+redisClient.on("disconnect", () => {
+  console.log("Redis is disconnected");
+});
+
+const inti = async () => {
+  // added hashSet
+  const res = await redisClient.hset("sde", {
+    name: "krish",
+    company: "Attlassian",
+    exp: "2+ year",
+    skill: "backend developer",
+  });
+  console.log(res);
+
+  await redisClient.hset("Job", {
+    role: "Sde",
+    openings: 10,
+    exp: "interns",
+    skill: "Nodejs",
+  });
+
+  // log property
+  const roleProperty = await redisClient.hget("Job", "role");
+  console.log(roleProperty);
+};
+
+inti();

f773cad29ab6f641f0325fef78127a4550bc9245
Krish Singh <154358180+krishsingh120@users.noreply.github.com>
2026-01-08 21:15:57 +0530

    Added streams in redis

---
+++ redisStreams.js
@@ -0,0 +1,31 @@
+const redisClient = require("./redisClient");
+
+// on "connect" -> event
+redisClient.on("connect", () => {
+  console.log("Redis is connected");
+});
+
+// on "disconnect" -> event
+redisClient.on("end", () => {
+  console.log("Redis is disconnected");
+});
+
+const inti = async () => {
+  const res = await redisClient.xadd(
+    "race:france",
+    "*",
+    "rider",
+    "Castilla",
+    "speed",
+    "30.2",
+    "position",
+    "1",
+    "location_id",
+    "1"
+  );
+
+  // timestamp-based ID -> 1767887069162-0
+  console.log("Stream Entry ID:", res);
+};
+
+inti();
